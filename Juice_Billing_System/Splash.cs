8c49997a37d1b1ff0f5a361e7d1fa92700c6dce2
vaibhavawasarkar <vs.scala.619@gmail.com>
2023-10-17 15:48:00 +0530

    Add project files.

---
+++ Juice_Billing_System/Splash.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Juice_Billing_System
+{
+    public partial class Splash : Form
+    {
+        public Splash()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
